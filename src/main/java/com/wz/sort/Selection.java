d23acf70474639671d595ee6431156b5eabd508a
wangzi <wangzi>
2018-05-01 14:00:12 +0800

    selection

---
+++ src/main/java/com/wz/sort/Selection.java
@@ -0,0 +1,43 @@
+/**
+ * <p>Title: Selection</p>
+ * <p>Description: </p>
+ * <p>Created by wangzi on 2018/5/1</p>
+ * <p>Emil: ustcwangzi@foxmail.com</p>
+ * <p>WebSite: https://github.com/ustcwangzi/</p>
+ */
+package com.wz.sort;
+
+import com.wz.utils.SortUtils;
+
+/**
+ * <p>选择排序</p>
+ * <p>
+ *     1、找出数组中最小的那个元素，将它和数组的第一个元素交换位置
+ *     2、在剩下的元素中找出最小的元素，将它和数组的第二个元素交换位置
+ *     3、依此类推，直到将整个数组排序
+ * </p>
+ * <p>长度为N的数组，大约需要N(N-1)2次比较和N次交换</p>
+ *
+ * @author wangzi
+ */
+public class Selection {
+    public static void sort(Comparable[] arr){
+        int N = arr.length;
+        for (int i = 0; i < N; i++){
+            int min = i; //最小元素的索引
+            for (int j = i+1; j < N; j++){
+                if (SortUtils.less(arr[j], arr[min])){
+                    min = j;
+                }
+            }
+            SortUtils.exch(arr, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] arr = new String[]{"a", "n", "i", "p", "z"};
+        sort(arr);
+        assert SortUtils.isSorted(arr);
+        SortUtils.show(arr);
+    }
+}
---
+++ src/main/java/com/wz/utils/SortUtils.java
@@ -0,0 +1,54 @@
+/**
+ * <p>Title: SortUtils</p>
+ * <p>Description: </p>
+ * <p>Created by wangzi on 2018/5/1</p>
+ * <p>Emil: ustcwangzi@foxmail.com</p>
+ * <p>WebSite: https://github.com/ustcwangzi/</p>
+ */
+package com.wz.utils;
+
+import java.util.Arrays;
+
+/**
+ * <p>排序工具类</p>
+ *
+ * @author wangzi
+ */
+public class SortUtils {
+    /**
+     * 元素比较
+     * self < other : true
+     */
+    public static boolean less(Comparable self, Comparable other){
+        return self.compareTo(other) < 0;
+    }
+
+    /**
+     * 元素交换
+     */
+    public static void exch(Comparable[] arr, int i, int j){
+        Comparable t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    /**
+     * 单行输出元素
+     */
+    public static void show(Comparable[] arr){
+        Arrays.asList(arr).forEach(a -> System.out.print(a + " "));
+        System.out.println();
+    }
+
+    /**
+     * 元素是否有序
+     */
+    public static boolean isSorted(Comparable[] arr){
+        for (int i = 1; i< arr.length; i++){
+            if (less(arr[i], arr[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
