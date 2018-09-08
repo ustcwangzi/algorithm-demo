7b943c862190d52a381436949d9ea89177608b3c
wangzi <ustcwangzi@foxmail.com>
2018-09-08 15:02:49 +0800

    DijkstraAlgorithm

--- src/main/java/com/wz/grokking/DijkstraAlgorithm.java
+++ src/main/java/com/wz/grokking/DijkstraAlgorithm.java
@@ -7,13 +7,18 @@
  */
 package com.wz.grokking;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * <p>Dijkstra算法</p>
+ * <p>
+ *     初始时，parentMap就是与source直接相邻的节点
+ *     distanceMap就是与source直接相邻的节点的距离
+ *     然后遍历distanceMap寻找最短路径，将其和已知最短路径作比较，
+ *     更小的话则更新distanceMap和parentMap
+ *     每处理完一个节点后，记录下来，后续的处理中不会再将这个节点考虑进去
+ *     重复以上过程，得到最终的最短路径
+ * </p>
  * <p>
  *     Dijkstra算法不能处理含有"负权值"的图
  *     因为当一个图含有负权值时，处理新节点时可能会更新已经"处理过的"节点
@@ -26,28 +31,108 @@ public class DijkstraAlgorithm {
     /**
      * 表示图
      */
-    private static Map<Integer, Map<Integer, Double>> graph = new HashMap<>();
+    private static Map<String, Map<String, Double>> graph = new HashMap<>();
+    /**
+     * 保存已知路径的父节点
+     */
+    private static Map<String, String> parentMap = new HashMap<>();
     /**
      * 已处理过的的节点
      */
-    private static List<Integer> processed = new ArrayList<>();
+    private static List<String> processed = new ArrayList<>();
+    /**
+     * 最短距离
+     */
+    private static Map<String, Double> distanceMap = new HashMap<>();
 
-    private static void findShortestPath(int source, int target){
+    private static void findShortestPath(String source, String target) {
+        distanceMap.putAll(graph.get(source));
+        for (Map.Entry<String, Map<String, Double>> entry : graph.entrySet()) {
+            distanceMap.putIfAbsent(entry.getKey(), Double.POSITIVE_INFINITY);
+            for (String key : entry.getValue().keySet()) {
+                parentMap.put(key, entry.getKey());
+                distanceMap.putIfAbsent(key, Double.POSITIVE_INFINITY);
+            }
+        }
 
+        String node = findShortestDistancesNode(distanceMap);
+        while (node != null) {
+            // 到达终点
+            if (node.equals(target)) {
+                distanceMap.put(source, 0.0);
+                return;
+            }
+
+            // 已知最短路径
+            Double distance = distanceMap.get(node);
+            Map<String, Double> neighbors = graph.get(node);
+            for (String key : neighbors.keySet()) {
+                double newDistance = distance + neighbors.get(key);
+                // 更新最短路径
+                if (distanceMap.get(key) > newDistance) {
+                    distanceMap.put(key, newDistance);
+                    parentMap.put(key, node);
+                }
+            }
+            // 已处理节点，后续不会再次处理
+            processed.add(node);
+            node = findShortestDistancesNode(distanceMap);
+        }
     }
 
-    private static Integer findShortestDistancesNode(Map<Integer, Double> distances){
+    /**
+     * 遍历相邻节点，寻找最短路径
+     */
+    private static String findShortestDistancesNode(Map<String, Double> distances) {
         Double shortestDistance = Double.POSITIVE_INFINITY;
-        Integer shortestDistanceNode = null;
+        String shortestDistanceNode = null;
 
-        for (Map.Entry<Integer, Double> node : distances.entrySet()){
+        for (Map.Entry<String, Double> node : distances.entrySet()) {
             double distance = node.getValue();
-            if (distance < shortestDistance && !processed.contains(node.getKey())){
+            if (distance < shortestDistance && !processed.contains(node.getKey())) {
                 shortestDistance = distance;
                 shortestDistanceNode = node.getKey();
             }
         }
 
         return shortestDistanceNode;
+    }
+
+    private static void printPath(String source, String target) {
+        Stack<String> pathStack = new Stack<>();
+        pathStack.push(target);
+        String w = parentMap.get(target);
+        while (!w.equals(source)) {
+            pathStack.push(w);
+            w = parentMap.get(w);
+        }
+        pathStack.push(source);
+
+        while (!pathStack.empty()) {
+            String path = pathStack.pop();
+            if (path.equals(target)) {
+                System.out.print(path);
+            } else {
+                System.out.print(path + " -> ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        graph.put("A", new HashMap<>());
+        graph.get("A").put("B", 6.0);
+        graph.get("A").put("C", 2.0);
+
+        graph.put("B", new HashMap<>());
+        graph.get("B").put("D", 1.0);
+
+        graph.put("C", new HashMap<>());
+        graph.get("C").put("B", 3.0);
+        graph.get("C").put("D", 5.0);
+
+        String source = "A", target = "D";
+        findShortestPath(source, target);
+        System.out.println(distanceMap);
+        printPath(source, target);
     }
 }
