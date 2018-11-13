f8cbf6d442838b99e8229c10e17706e8206a0240
wangzi <ustcwangzi@foxmail.com>
2018-11-13 09:20:13 +0800

    CoinsMin

--- src/main/java/com/wz/recursionanddynamicprogramming/CoinsMin.java
+++ src/main/java/com/wz/recursionanddynamicprogramming/CoinsMin.java
@@ -32,6 +32,7 @@ public class CoinsMin {
             }
         }
 
+        // 左边某个位置的值
         int left;
         for (int i = 1; i < array.length; i++) {
             for (int j = 1; j <= aim; j++) {
@@ -63,6 +64,7 @@ public class CoinsMin {
             }
         }
 
+        // 左边某个位置的值
         int left;
         // 按行更新result
         for (int i = 1; i < array.length; i++) {
@@ -74,6 +76,66 @@ public class CoinsMin {
                 result[j] = Math.min(left, result[j]);
             }
         }
+
+        return result[aim] != MAX ? result[aim] : -1;
+    }
+
+    public static int uniMinCoinsOne(int[] array, int aim) {
+        if (array == null || array.length == 0 || aim < 0) {
+            return -1;
+        }
+
+        int[][] result = new int[array.length][aim + 1];
+        // 第一行只能使用一张array[0]，只有result[0][array[0]]=1，其他均为MAX
+        for (int j = 1; j <= aim; j++) {
+            result[0][j] = MAX;
+        }
+        if (array[0] <= aim) {
+            result[0][array[0]] = 1;
+        }
+
+        // 左上角某个位置的值
+        int leftUp;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                leftUp = MAX;
+                if (j - array[i] >= 0 && result[i - 1][j - array[i]] != MAX) {
+                    leftUp = result[i - 1][j - array[i]] + 1;
+                }
+                result[i][j] = Math.min(leftUp, result[i - 1][j]);
+            }
+        }
+
+        return result[array.length - 1][aim] != MAX ? result[array.length - 1][aim] : -1;
+    }
+
+    public static int uniMinCoinsTwo(int[] array, int aim) {
+        if (array == null || array.length == 0 || aim < 0) {
+            return -1;
+        }
+
+        int[] result = new int[aim + 1];
+        // 初始化只能使用一张array[0]，只有result[0][array[0]]=1，其他均为MAX
+        for (int j = 1; j <= aim; j++) {
+            result[j] = MAX;
+        }
+        if (array[0] <= aim) {
+            result[array[0]] = 1;
+        }
+
+        // 左上角某个位置的值
+        int leftUp;
+        for (int i = 1; i < array.length; i++) {
+            // 每个货币只能使用一张，需要从右向左更新，不然会导致每个货币使用多张
+            for (int j = aim; j > 0; j--) {
+                leftUp = MAX;
+                if (j - array[i] >= 0 && result[j - array[i]] != MAX) {
+                    leftUp = result[j - array[i]] + 1;
+                }
+                result[j] = Math.min(leftUp, result[j]);
+            }
+        }
+
         return result[aim] != MAX ? result[aim] : -1;
     }
 
@@ -82,5 +144,7 @@ public class CoinsMin {
         int aim = 10;
         System.out.println(minCoinsOne(array, aim));
         System.out.println(minCoinsTwo(array, aim));
+        System.out.println(uniMinCoinsOne(array, aim));
+        System.out.println(uniMinCoinsTwo(array, aim));
     }
 }
