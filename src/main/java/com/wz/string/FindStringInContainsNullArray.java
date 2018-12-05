cdac20845f34f1e893325b8de6d3759c88e5d2c8
wangzi <ustcwangzi@foxmail.com>
2018-12-05 08:31:48 +0800

    FindStringInContainsNullArray

--- src/main/java/com/wz/string/FindStringInContainsNullArray.java
+++ src/main/java/com/wz/string/FindStringInContainsNullArray.java
@@ -9,6 +9,23 @@ package com.wz.string;
 
 /**
  * <p>在有序但包含空的数组中查找字符串</p>
+ * <p>
+ *     给定一个字符串数组array[]，在array中有些位置为null，但在不为null的位置上，其字符串是按照字典顺序由小到大出现的。
+ *     在给定一个字符串target，查找target在array中出现的左侧的位置，target为null时返回-1，找不到时返回-1。
+ *     解决方案：
+ *        尽可能多的采用二分查找，过程如下：
+ *        1、在array[left...right]上进行查找，result表示target在array中最左的位置，初始时left=0，right=array.length-1，result=-1
+ *        2、令mid=(left+right)/2，array[mid]为array[left...right]中间位置的字符串
+ *        3、如果array[mid]与target相等，说明找到了target，令result=mid，但要找的是最左的位置，所以还要在左半区查找，
+ *           看有没有更左的target出现，令right=mid-1，然后重复步骤二
+ *        4、如果array[mid]与target不等，且array[mid]!=null
+ *        4.1、若array[mid]小于target，说明整个左半区不会出现target，在右半区查找，令left=mid+1，然后重复步骤二
+ *        4.2、若array[mid]大于target，说明整个右半区不会出现target，在左半区查找，令right=mid-1，然后重复步骤二
+ *        5、如果array[mid]与target不等，且array[mid]==null，此时从mid开始，从右到左遍历左半区，到第一个不为null的位置i停止
+ *        5.1、如果i < left，说明整个左半区都为null，在右半区查找，令left=mid+1，然后重复步骤二
+ *        5.2、如果array[i]小于target，说明整个左半区不会出现target，在右半区查找，令left=mid+1，然后重复步骤二
+ *        5.3、如果array[i]等于target，说明找到了target，令result=i，但要找的是最左的位置，令right=i-1，然后重复步骤二。
+ * </p>
  *
  * @author wangzi
  */
