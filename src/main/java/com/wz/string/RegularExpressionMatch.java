0c99f4390113c836699bb96736285ed2bb40118c
wangzi <ustcwangzi@foxmail.com>
2018-12-16 11:40:02 +0800

    isMatch

--- src/main/java/com/wz/string/RegularExpressionMatch.java
+++ src/main/java/com/wz/string/RegularExpressionMatch.java
@@ -56,9 +56,65 @@ public class RegularExpressionMatch {
         return true;
     }
 
+    public static boolean isMatchDP(String str, String exp) {
+        if (str == null || exp == null) {
+            return false;
+        }
+
+        char[] strArray = str.toCharArray();
+        char[] expArray = exp.toCharArray();
+        if (!isValid(strArray, expArray)) {
+            return false;
+        }
+        // 动态规划矩阵
+        boolean[][] dp = initDPMap(strArray, expArray);
+        for (int i = strArray.length - 1; i > -1; i--) {
+            for (int j = expArray.length - 2; j > -1; j--) {
+                if (expArray[j + 1] != '*') {
+                    dp[i][j] = (strArray[i] == expArray[j] || expArray[j] == '.')
+                            && dp[i + 1][j + 1];
+                } else {
+                    int strIndex = i;
+                    while (strIndex != strArray.length && (strArray[strIndex] == expArray[j] || expArray[j] == '.')) {
+                        if (dp[strIndex][j + 2]) {
+                            dp[i][j] = true;
+                            break;
+                        }
+                        strIndex++;
+                    }
+                    if (!dp[i][j]) {
+                        dp[i][j] = dp[strIndex][j + 2];
+                    }
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    private static boolean[][] initDPMap(char[] str, char[] exp) {
+        int strLen = str.length;
+        int expLen = exp.length;
+        boolean[][] dp = new boolean[strLen + 1][expLen + 1];
+        dp[strLen][expLen] = true;
+        for (int j = expLen - 2; j > -1; j = j - 2) {
+            if (exp[j] != '*' && exp[j + 1] == '*') {
+                dp[strLen][j] = true;
+            } else {
+                break;
+            }
+        }
+        if (strLen > 0 && expLen > 0) {
+            if (exp[expLen - 1] == '.' || str[strLen - 1] == exp[expLen - 1]) {
+                dp[strLen - 1][expLen - 1] = true;
+            }
+        }
+        return dp;
+    }
+
     public static void main(String[] args) {
         String str = "abcccdefg";
         String exp = "ab.*d.*e.*";
         System.out.println(isMatch(str, exp));
+        System.out.println(isMatchDP(str, exp));
     }
 }
