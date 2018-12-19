a6ffd02017d9ee58213eaa1e997bd5b222dff437
wangzi <ustcwangzi@foxmail.com>
2018-12-19 08:44:59 +0800

    GetMax

--- src/main/java/com/wz/bitoperation/GetMax.java
+++ src/main/java/com/wz/bitoperation/GetMax.java
@@ -9,6 +9,23 @@ package com.wz.bitoperation;
 
 /**
  * <p>不用任何比较判断找出两个数中较大的数</p>
+ * <p>
+ *     给定两个32为整数a和b，不用任何比较判断，返回a和b中较大的。
+ *     方案一：
+ *        根据a-b的符号，判断两者的大小。sign()返回整数n的符号，正数和0返回1，负数返回0；flip()如果n为1，返回0，如果n为0，返回1。
+ *        所以，如果a-b为正数或0，则signC为1，flip(signC)为0；如果a-b为负数，则signC为0，flip(signC)为1。
+ *        即signC和flip(signC)必然一个为0，另一个为1，所以返回 a*signC+b*flip(signC)
+ *        方案一有局限性，就是如果a-b的值出现溢出，则返回的结果不正确。
+ *     方案二：
+ *        方案二可以彻底解决溢出问题。
+ *        如果a与b的符号不同，此时diffSignAB=1，sameSignAB=0，有：
+ *          如果a为0或正数，那么b为负数，signA=1，signB=0，应该返回a；
+ *          如果a为负数，那么b为0或正数，signA=0，signB=1，应该返回b。
+ *        如果a与b的符号相同，此时diffSignAB=0，sameSignAB=1，这种情况下a-b的值绝不会溢出，有：
+ *          如果a-b为0或正数，signC=1，应该返回a；
+ *          如果a-b为负数，signC=0，应该返回b。
+ *        综上所述，应该返回 a*(diffSignAB*signA+sameSignAB*signC)+b*flip(diffSignAB*signA+sameSignAB*signC)
+ * </p>
  *
  * @author wangzi
  */
