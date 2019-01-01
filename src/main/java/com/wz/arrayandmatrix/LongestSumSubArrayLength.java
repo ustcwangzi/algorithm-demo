c2f83ead071d2b3c6f05cf29b1566793bea7f9bc
wangzi <ustcwangzi@foxmail.com>
2019-01-01 10:20:12 +0800

    LongestSumSubArrayLength

--- src/main/java/com/wz/arrayandmatrix/LongestSumSubArrayLength.java
+++ src/main/java/com/wz/arrayandmatrix/LongestSumSubArrayLength.java
@@ -7,6 +7,9 @@
  */
 package com.wz.arrayandmatrix;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <p>无序数组中累加和为给定值的最长子数组长度</p>
  *
@@ -43,8 +46,39 @@ public class LongestSumSubArrayLength {
         return result;
     }
 
+    /**
+     * 无序数组中累加和为k的最长子数组长度
+     */
+    public static int getMaxLengthInArray(int[] array, int k) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+
+        // 记录累加和key首次出现的位置value
+        Map<Integer, Integer> map = new HashMap<>();
+        // 以免漏掉从0开始的子数组
+        map.put(0, -1);
+        // array[0...i]的累加和
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            // s(i)为sum
+            sum += array[i];
+            // s(j)为sum-k，array[j+1...i]的累加=s(i)-s(j)=k
+            if (map.containsKey(sum - k)) {
+                result = Math.max(i - map.get(sum - k), result);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] array1 = {1, 2, 1, 1, 1};
         System.out.println(getMaxLengthInPositiveArray(array1, 3));
+        int[] array2 = {1, 2, 3, 3};
+        System.out.println(getMaxLengthInArray(array2, 6));
     }
 }
