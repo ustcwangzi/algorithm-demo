a51003ba5af693056d3f2cd1501260b7b8423108
wangzi <ustcwangzi@foxmail.com>
2019-01-27 14:55:05 +0800

    FindNumberInSortedRotateArray

--- src/main/java/com/wz/others/FindNumberInSortedRotateArray.java
+++ src/main/java/com/wz/others/FindNumberInSortedRotateArray.java
@@ -9,6 +9,22 @@ package com.wz.others;
 
 /**
  * <p>在有序旋转数组中找到一个数</p>
+ * <p>
+ *     有序数组array可能经过一次旋转处理，也可能没有，且array可能存在重复的数。
+ *     例如[1,2,3,4,5,6,7]，可以旋转成[4,5,6,7,1,2,3]等。给定一个可能旋转过的有序数组array，再给定一个数n，返回array中是否含有n。
+ *     解决方案：
+ *        如果一个有序数组经过旋转后，只有一个位置会出现降序，其余部分都是升序。可使用二分查找法。
+ *        初始时，low=0，high=array.length-1，mid=(low+high)/2。
+ *        1、在array[low] != array[mid]的情况下，如果array[low] < array[mid]，说明array[low...mid]是有序的，
+ *           如果number大于array[low]并且小于array[mid]，说明number在左半区，令high = mid-1；
+ *           如果number不在array[low...mid]区间，则一定在右半区，令low = mid+1。
+ *        2、在array[mid] != array[high]的情况下，如果array[mid] < array[high]，说明array[mid...high]是有序的，
+ *           如果number大于array[mid]并且小于array[high]，说明number在右半区，令low = mid+1；
+ *           如果number不在array[mid...high]区间，则一定在左半区，令high = mid-1。
+ *        3、在array数组中可能存在重复的值，那么就可能发生array[low] == array[mid] == array[high]的情况，
+ *           此时从low位置开始向右遍历，直到array[low] != array[mid]。
+ *           如果遍历到mid位置都一直与array[mid]相等，说明左半区都是一个值，所以number一定出现在右半区，令low = mid + 1。
+ * </p>
  *
  * @author wangzi
  */
