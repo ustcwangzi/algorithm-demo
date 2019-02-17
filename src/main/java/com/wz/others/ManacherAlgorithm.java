a8b45e5e35e51dd6a1ab7086e0d0aa0c5c9493fa
wangzi <ustcwangzi@foxmail.com>
2019-02-17 12:22:45 +0800

    ManacherAlgorithm

--- src/main/java/com/wz/others/ManacherAlgorithm.java
+++ src/main/java/com/wz/others/ManacherAlgorithm.java
@@ -14,12 +14,15 @@ package com.wz.others;
  */
 public class ManacherAlgorithm {
 
+    /**
+     * 最长回文子串的长度
+     */
     public static int longestPalindromeLength(String str) {
         if (str == null || str.length() == 0) {
             return 0;
         }
         char[] manacherArray = manacherArray(str);
-        // 回文半径数组，radiusArray[i]-1 正好是原字符串中i对应的字符的最长回文串长度
+        // 回文半径数组，radiusArray[i]-1 正好是该回文子串在原字符串中的长度
         int[] radiusArray = new int[manacherArray.length];
         // 最右回文右边界
         int right = -1;
@@ -48,6 +51,52 @@ public class ManacherAlgorithm {
         return max - 1;
     }
 
+    /**
+     * 末尾添加最短字符串变成回文串
+     */
+    public static String shortestEnd(String str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        char[] manacherArray = manacherArray(str);
+        // 回文半径数组，radiusArray[i]-1 正好是该回文子串在原字符串中的长度
+        int[] radiusArray = new int[manacherArray.length];
+        // 最右回文右边界
+        int right = -1;
+        // 最右回文右边界right的对称中心
+        int center = -1;
+        // 最右回文右边界到达最后位置时的回文半径
+        int maxContainsEndRadius = -1;
+        for (int i = 0; i < manacherArray.length; i++) {
+            // i以center为中心的对称点为 2*center-i
+            radiusArray[i] = right > i ? Math.min(radiusArray[2 * center - i], right - 1) : 1;
+            while (i + radiusArray[i] < manacherArray.length && i - radiusArray[i] > -1) {
+                // 以i为中心，向左右扩展
+                if (manacherArray[i + radiusArray[i]] == manacherArray[i - radiusArray[i]]) {
+                    radiusArray[i]++;
+                } else {
+                    break;
+                }
+            }
+            // 更新最右边界和对称中心
+            if (i + radiusArray[i] > right) {
+                right = i + radiusArray[i];
+                center = i;
+            }
+            // 最右回文边界已到达最后位置
+            if (right == manacherArray.length) {
+                maxContainsEndRadius = radiusArray[i];
+                break;
+            }
+        }
+        char[] result = new char[str.length() - maxContainsEndRadius + 1];
+        for (int i = 0; i < result.length; i++) {
+            // 将之前不是最长回文子串的部分逆序
+            result[result.length - 1 - i] = manacherArray[i * 2 + 1];
+        }
+        return String.valueOf(result);
+    }
+
     private static char[] manacherArray(String str) {
         char[] array = str.toCharArray();
         char[] result = new char[str.length() * 2 + 1];
@@ -62,7 +111,9 @@ public class ManacherAlgorithm {
     public static void main(String[] args) {
         String str = "abc1234321ab";
         System.out.println(longestPalindromeLength(str));
+        System.out.println(shortestEnd(str));
         str = "abcd123321";
         System.out.println(longestPalindromeLength(str));
+        System.out.println(shortestEnd(str));
     }
 }
