df8eb91a3314b7ba1f8d34553e8cb72dfa8ff892
wangzi <ustcwangzi@foxmail.com>
2019-02-20 08:29:49 +0800

    ThrowChessPieces

--- src/main/java/com/wz/others/ThrowChessPieces.java
+++ src/main/java/com/wz/others/ThrowChessPieces.java
@@ -104,10 +104,105 @@ public class ThrowChessPieces {
         return curArray[n];
     }
 
+    /**
+     * 四边形不等式
+     */
+    public static int solutionFour(int n, int k) {
+        if (n < 1 || k < 1) {
+            return 0;
+        }
+        if (k == 1) {
+            return n;
+        }
+
+        int[][] dp = new int[n + 1][k + 1];
+        // 只使用一个棋子
+        for (int i = 1; i != dp.length; i++) {
+            dp[i][1] = i;
+        }
+
+        // 最优解
+        int[] levels = new int[k + 1];
+        // 只有一层
+        for (int i = 1; i != dp[0].length; i++) {
+            dp[1][i] = 1;
+            levels[i] = 1;
+        }
+
+        // i层j个棋子时的次数
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = k; j > 1; j--) {
+                int min = Integer.MAX_VALUE;
+                int minEnum = levels[j];
+                // 棋子变少之后，第一个棋子尝试楼层的上限可以确定
+                // 首次在中间楼层开始，后面取上次的最优解
+                int maxEnum = j == k ? i / 2 + 1 : levels[j + 1];
+                // minEnum～maxEnum层逐个尝试最优解
+                for (int h = minEnum; h < maxEnum + 1; h++) {
+                    int cur = Math.max(dp[h - 1][j - 1], dp[i - h][j]);
+                    if (cur <= min) {
+                        min = cur;
+                        levels[j] = h;
+                    }
+                }
+                dp[i][j] = min + 1;
+            }
+        }
+        return dp[n][k];
+    }
+
+    /**
+     * 最优解，i个棋子最多高端的楼层数
+     */
+    public static int solutionFive(int n, int k) {
+        if (n < 1 || k < 1) {
+            return 0;
+        }
+        // 完全二分方式所需次数
+        int bsTimes = log2N(n) + 1;
+        if (k >= bsTimes) {
+            return bsTimes;
+        }
+
+        // i个棋子最多搞定的楼层数
+        int[] dp = new int[k];
+        int result = 0;
+        while (true) {
+            result++;
+            // dp[i-1]
+            int previous = 0;
+            for (int i = 0; i < dp.length; i++) {
+                int tmp = dp[i];
+                dp[i] = dp[i] + previous + 1;
+                previous = tmp;
+                if (dp[i] >= n) {
+                    return result;
+                }
+            }
+        }
+    }
+
+    private static int log2N(int n) {
+        int result = -1;
+        while (n != 0) {
+            result++;
+            n >>>= 1;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int n = 21, k = 2;
         System.out.println(solutionOne(n, k));
         System.out.println(solutionTwo(n, k));
         System.out.println(solutionThree(n, k));
+        System.out.println(solutionFour(n, k));
+        System.out.println(solutionFive(n, k));
+
+        n = 105;
+        System.out.println(solutionTwo(n, k));
+        System.out.println(solutionThree(n, k));
+        System.out.println(solutionFour(n, k));
+        System.out.println(solutionFive(n, k));
     }
 }
