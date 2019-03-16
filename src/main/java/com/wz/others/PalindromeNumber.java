f189c5e0fd6d91a28d8f59c7dd9821afeb1c764e
wangzi <ustcwangzi@foxmail.com>
2019-03-16 09:03:47 +0800

    PalindromeNumber

--- src/main/java/com/wz/others/PalindromeNumber.java
+++ src/main/java/com/wz/others/PalindromeNumber.java
@@ -14,7 +14,7 @@ package com.wz.others;
  *     解决方案：
  *        1、生成变量help，初始时help=1，用help不断乘以10，直到变得和number的位数一样，例如number=131时，help=100
  *        2、此时number/help就是最高位数字，number%10就是最低位数字，比较这两个数字，不相同直接返回false；
- *           相同则令number=(number%hemp)/10，即number变成除去最高位和最低位两个数字之后的值，
+ *           相同则令number=(number%help)/10，即number变成除去最高位和最低位两个数字之后的值，
  *           同时令help/=100，即让help与number的位数保持一致
  *        3、如果number==0，表示所有的数字均已完成判断，返回true，否则重复步骤二。
  *        总之，就是让number每次去除最左和最右两个数，然后逐渐完成所有对应的判断，如果number是负数时，使用其绝对值进行判断。
@@ -43,7 +43,7 @@ public class PalindromeNumber {
             }
             // 去除最高位和最低位
             number = (number % help) / 10;
-            number /= 100;
+            help /= 100;
         }
         return true;
     }
