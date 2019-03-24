fd5a93183bc94f708741d28e3a39cdb40f22cc48
wangzi <ustcwangzi@foxmail.com>
2019-03-24 10:19:12 +0800

    MaxAbsoluteBetweenLeftAndRightTest

---
+++ src/test/java/com/wz/MaxAbsoluteBetweenLeftAndRightTest.java
@@ -0,0 +1,43 @@
+/**
+ * <p>Title: MaxAbsoluteBetweenLeftAndRightTest</p>
+ * <p>Description: </p>
+ * <p>Created by wangzi on 2019/3/24</p>
+ * <p>Emil: ustcwangzi@foxmail.com</p>
+ * <p>WebSite: https://github.com/ustcwangzi/</p>
+ */
+package com.wz;
+
+import com.wz.others.MaxAbsoluteBetweenLeftAndRight;
+
+import java.util.Arrays;
+
+/**
+ * <p>数组划分中leftMax与rightMax之差的最大值</p>
+ *
+ * @author wangzi
+ */
+public class MaxAbsoluteBetweenLeftAndRightTest {
+    private static int solution(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int cur : array) {
+            max = Math.max(max, cur);
+        }
+
+        return max - Math.min(array[0], array[array.length - 1]);
+    }
+
+    public static void main(String[] args) {
+        int times = 100;
+        boolean result = true;
+        for (int i = 0; i < times; i++) {
+            int[] array = RandomUtils.genRandomArray();
+            if (solution(array) != MaxAbsoluteBetweenLeftAndRight.maxAbsoluteThree(array)) {
+                result = false;
+                System.out.println("Error, array:" + Arrays.toString(array));
+            }
+        }
+        if (result) {
+            System.out.println("Past");
+        }
+    }
+}
