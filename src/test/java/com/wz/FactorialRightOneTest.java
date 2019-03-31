0378e42cc1e738bbc55cddcfe27145fe1ca248e8
wangzi <ustcwangzi@foxmail.com>
2019-03-31 09:41:38 +0800

    FactorialRightOneTest

--- src/test/java/com/wz/FactorialRightOneTest.java
+++ src/test/java/com/wz/FactorialRightOneTest.java
@@ -1,5 +1,5 @@
 /**
- * <p>Title: FactorialEndWithZeroTest</p>
+ * <p>Title: FactorialRightOneTest</p>
  * <p>Description: </p>
  * <p>Created by wangzi on 2019/3/31</p>
  * <p>Emil: ustcwangzi@foxmail.com</p>
@@ -10,16 +10,16 @@ package com.wz;
 import com.wz.others.Factorial;
 
 /**
- * <p>N阶乘中末尾有多少个0，等价于N中有多少个因子5</p>
+ * <p>N阶乘二进制表达式中最右侧1的位置，最右位置为0，只要出现一个因子2，最低位的1就会向左移动一位</p>
  *
  * @author wangzi
  */
-public class FactorialEndWithZeroTest {
+public class FactorialRightOneTest {
     private static int solution(int n) {
         int result = 0;
         while (n > 0) {
-            result += n / 5;
-            n /= 5;
+            result += n / 2;
+            n /= 2;
         }
         return result;
     }
@@ -29,8 +29,8 @@ public class FactorialEndWithZeroTest {
         boolean result = true;
         for (int i = 0; i < times; i++) {
             int number = RandomUtils.genRandomNumber() + 1;
-            int count = solution(number);
-            if (count != Factorial.zeroNumOne(number) || count != Factorial.zeroNumTwo(number)) {
+            int position = solution(number);
+            if (position != Factorial.rightOneOne(number) || position != Factorial.rightOneTwo(number)) {
                 result = false;
                 System.out.println("Error, number:" + number);
             }
