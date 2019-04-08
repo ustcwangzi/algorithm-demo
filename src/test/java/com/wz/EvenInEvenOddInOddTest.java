dbce601fd0fe868e32549a1eef008efb8d5ae3d7
wangzi <ustcwangzi@foxmail.com>
2019-04-08 08:05:17 +0800

    EvenInEvenOddInOddTest

--- src/test/java/com/wz/EvenInEvenOddInOddTest.java
+++ src/test/java/com/wz/EvenInEvenOddInOddTest.java
@@ -4,10 +4,17 @@ import com.wz.arrayandmatrix.EvenInEvenOddInOdd;
 
 import java.util.Arrays;
 
+/**
+ * <p>调整数组使奇数位置都是奇数或者偶数位置都是偶数</p>
+ *
+ * @author wangzi
+ */
 public class EvenInEvenOddInOddTest {
     private static int[] solution(int[] array) {
+        // even:已调整的最左偶数下标，odd:已调整的最左奇数下标
         int even = 0, odd = 1;
         int end = array.length - 1;
+        // 每次检查array[N-1]的奇偶，偶数则和even交换，奇数则和odd交换
         while (even <= end && odd <= end) {
             if ((array[end] & 1) == 0) {
                 swap(array, even, end);
@@ -21,6 +28,9 @@ public class EvenInEvenOddInOddTest {
     }
 
     private static void swap(int[] array, int self, int other) {
+        if (array[self] == array[other]) {
+            return;
+        }
         int tmp = array[self];
         array[self] = array[other];
         array[other] = tmp;
