996de31f5af078004808499d12528764f702b71d
wangzi <ustcwangzi@foxmail.com>
2019-04-11 08:00:58 +0800

    LongestSumSubPositiveArrayLengthTest

--- src/test/java/com/wz/LongestSumSubPositiveArrayLengthTest.java
+++ src/test/java/com/wz/LongestSumSubPositiveArrayLengthTest.java
@@ -4,21 +4,30 @@ import com.wz.arrayandmatrix.LongestSumSubArrayLength;
 
 import java.util.Arrays;
 
+/**
+ * <p>正数数组中累加和为给定值的最长子数组长度</p>
+ *
+ * @author wangzi
+ */
 public class LongestSumSubPositiveArrayLengthTest {
     private static int solution(int[] array, int k) {
         int left = 0, right = 0, result = 0;
+        // array[left...right]累加和
         int sum = array[0];
         while (right < array.length) {
             if (sum == k) {
+                // 当前和为k，要想再次为k，只有减少范围，即减去array[left]
                 result = Math.max(result, right - left + 1);
                 sum -= array[left++];
             } else if (sum < k) {
+                // 当前和小于k，需要继续向右，即加上array[right]
                 right++;
                 if (right == array.length) {
                     break;
                 }
                 sum += array[right];
             } else {
+                // 当前和大于k，需要减少范围，即减去array[left]
                 sum -= array[left++];
             }
         }
