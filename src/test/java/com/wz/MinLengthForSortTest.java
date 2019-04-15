1d2a6c2777e1da9bcddc9ee44342ee466e6670f5
wangzi <ustcwangzi@foxmail.com>
2019-04-15 08:03:30 +0800

    MinLengthForSortTest

--- src/test/java/com/wz/MinLengthForSortTest.java
+++ src/test/java/com/wz/MinLengthForSortTest.java
@@ -4,10 +4,16 @@ import com.wz.arrayandmatrix.MinLengthForSort;
 
 import java.util.Arrays;
 
+/**
+ * <p>无需数组中需要排序的最短子数组长度</p>
+ *
+ * @author wangzi
+ */
 public class MinLengthForSortTest {
     private static int solution(int[] array) {
         int leftIndex = -1;
         int min = array[array.length - 1];
+        // 从右到左遍历，找到出现的最小值min，及大于最小值的最左位置leftIndex
         for (int i = array.length - 2; i > -1; i--) {
             if (array[i] > min) {
                 leftIndex = i;
@@ -21,6 +27,7 @@ public class MinLengthForSortTest {
 
         int rightIndex = -1;
         int max = array[0];
+        // 从左到右遍历，找到出现的最大值max，及小于最大值的最右位置rightIndex
         for (int i = 1; i < array.length; i++) {
             if (array[i] < max) {
                 rightIndex = i;
