b53105453a6a462721cf09d5056ce1f248adbff8
wangzi <ustcwangzi@foxmail.com>
2019-04-16 08:30:35 +0800

    FindMinKNumbersInArrayTest

--- src/test/java/com/wz/FindMinKNumbersInArrayTest.java
+++ src/test/java/com/wz/FindMinKNumbersInArrayTest.java
@@ -5,6 +5,11 @@ import com.wz.arrayandmatrix.FindMinKNumbersInArray;
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ * <p>无数数组中找到最小的k个数</p>
+ *
+ * @author wangzi
+ */
 public class FindMinKNumbersInArrayTest {
     private static int[] solution(int[] array, int k) {
         int[] heap = new int[k];
@@ -14,6 +19,7 @@ public class FindMinKNumbersInArrayTest {
         }
 
         for (int i = k; i < array.length; i++) {
+            // 当前元素小于堆顶元素，加入堆，然后进行堆调整
             if (array[i] < heap[0]) {
                 heap[0] = array[i];
                 heapify(heap, 0);
@@ -35,22 +41,28 @@ public class FindMinKNumbersInArrayTest {
         }
     }
 
+    /**
+     * 从index开始对堆进行调整
+     */
     private static void heapify(int[] heap, int index) {
         int heapSize = heap.length;
         int left = index * 2 + 1;
         int right = index * 2 + 2;
         int largestIndex = index;
         while (left < heapSize) {
+            // 获取父节点和子节点中的最大值对应的索引
             if (heap[left] > heap[index]) {
                 largestIndex = left;
             }
             if (right < heapSize && heap[right] > heap[largestIndex]) {
                 largestIndex = right;
             }
+            // 如果最大值对应的索引还是index，说明不需要调整
             if (largestIndex == index) {
                 break;
             }
             swap(heap, index, largestIndex);
+            // index变化到子节点，继续向下调整
             index = largestIndex;
             left = index * 2 + 1;
             right = index * 2 + 2;
