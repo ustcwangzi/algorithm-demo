0d0c99a8cfbb6609d3e5efd8c539f59b6b2a59da
wangzi <ustcwangzi@foxmail.com>
2019-04-22 08:12:10 +0800

    SwapWithoutTmpTest

--- src/test/java/com/wz/SwapWithoutTmpTest.java
+++ src/test/java/com/wz/SwapWithoutTmpTest.java
@@ -1,11 +1,19 @@
 package com.wz;
 
+/**
+ * <p>不使用额外变量交换两个正数的值</p>
+ *
+ * @author wangzi
+ */
 public class SwapWithoutTmpTest {
 
     public static void main(String[] args) {
         int a = 5, b = 6;
+        // a变为c，b不变
         a = a ^ b;
+        // b变为a，a还是c
         b = a ^ b;
+        // a变为b，b还是a
         a = a ^ b;
         System.out.println("a: " + a + ", b: " + b);
     }
