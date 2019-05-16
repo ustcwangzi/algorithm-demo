0f151fb129391be0a782089402f98f16f39c7fd5
wangzi <ustcwangzi@foxmail.com>
2019-05-16 08:17:57 +0800

    EditCostTest

--- src/test/java/com/wz/EditCostTest.java
+++ src/test/java/com/wz/EditCostTest.java
@@ -4,20 +4,33 @@ import com.wz.recursionanddp.EditCost;
 
 import java.util.Random;
 
+/**
+ * <p>将self通过插入、删除、替换操作编辑成other的最小代价</p>
+ *
+ * @author wangzi
+ */
 public class EditCostTest {
     private static int solution(String self, String other, int ic, int dc, int rc) {
         char[] selfArray = self.toCharArray();
         char[] otherArray = other.toCharArray();
         int m = selfArray.length, n = otherArray.length;
+
+        // dp[i][j]表示将self[0...i-1]编辑成other[0...j-1]的最小代价
         int[][] dp = new int[m + 1][n + 1];
 
+        // 第一列表示将self[0...M-1]编辑成空的最小代价，直接删除
         for (int i = 1; i <= m; i++) {
             dp[i][0] = dc * i;
         }
+        // 第一行表示将空编辑成other[0...N-1]的最小代价，直接插入
         for (int j = 1; j <= n; j++) {
             dp[0][j] = ic * j;
         }
 
+        // 1、self[0...i-1]编辑到other[0...j-2]，再插入other[j-1]
+        // 2、self[0...i-1]删除self[0...i-1]变为self[0...i-2]，再编辑到other[0...j-1]
+        // 3、self[i-1]==other[j-1]，则直接由self[0...i-2]编辑到other[0...j-2]
+        // 4、self[i-1]!=other[j-1]，由self[0...i-2]编辑到other[0...j-2]，再将self[i-1]替换为other[j-1]
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (selfArray[i - 1] == otherArray[j - 1]) {
