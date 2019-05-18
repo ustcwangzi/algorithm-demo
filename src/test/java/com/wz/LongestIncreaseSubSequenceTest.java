c384227459bd6e10762db31bd9327dcc250c2943
wangzi <ustcwangzi@foxmail.com>
2019-05-18 10:37:53 +0800

    LongestIncreaseSubSequenceTest

--- src/test/java/com/wz/LongestIncreaseSubSequenceTest.java
+++ src/test/java/com/wz/LongestIncreaseSubSequenceTest.java
@@ -18,9 +18,12 @@ import java.util.Arrays;
  */
 public class LongestIncreaseSubSequenceTest {
     private static int[] solution(int[] array) {
+        // dp[i]表示以array[i]为结尾的最长递增子序列长度
         int[] dp = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             dp[i] = 1;
+            // 在array[0...i-1]上找到小于array[i]的元素，并且以该元素结尾的最长递增子序列长度最大
+            // 该元素的最长递增子序列长度加一就是dp[i]
             for (int k = 0; k < i; k++) {
                 if (array[k] < array[i]) {
                     dp[i] = Math.max(dp[i], dp[k] + 1);
@@ -28,6 +31,7 @@ public class LongestIncreaseSubSequenceTest {
             }
         }
 
+        // 找到最长递增子序列的最大长度及位置
         int max = 0, end = 0;
         for (int i = 0; i < dp.length; i++) {
             if (dp[i] > max) {
@@ -38,6 +42,8 @@ public class LongestIncreaseSubSequenceTest {
 
         int[] result = new int[max];
         result[--max] = array[end];
+        // 从最大长度的位置逆序遍历，逐步找到每个元素
+        // 该元素值小于当前已找到的最大值、并且长度为当前长度减一
         for (int i = end; i > -1; i--) {
             if (array[i] < array[end] && dp[i] == dp[end] - 1) {
                 result[--max] = array[i];
