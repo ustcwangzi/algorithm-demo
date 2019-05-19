6cd77f0cc7bc9373deed23c6232d4e79ab75a2b7
wangzi <ustcwangzi@foxmail.com>
2019-05-19 10:24:01 +0800

    CoinsWayTest

--- src/test/java/com/wz/CoinsWayTest.java
+++ src/test/java/com/wz/CoinsWayTest.java
@@ -19,13 +19,21 @@ import java.util.Random;
  */
 public class CoinsWayTest {
     private static int solution(int[] array, int target) {
+        // dp[i][j]表示使用array[0...i]组成的j的方法数
         int[][] dp = new int[array.length][target + 1];
+
+        // 第一列，组成0任何货币都不使用
         for (int i = 0; i < array.length; i++) {
             dp[i][0] = 1;
         }
+        // 第一行，使用array[0]组成j，j能够被array[0]整除才行
         for (int j = 1; j * array[0] <= target; j++) {
             dp[0][j * array[0]] = 1;
         }
+        // 组成dp[i][j]，可以不使用array[i]，此时方法数为dp[i-1][j]
+        // 只使用一张array[i]，此时方法数为dp[i-1][j-array[i]]
+        // 只使用k张array[i]，此时方法数为dp[i-1][j-k*array[i]]
+        // 以上各种可能相加，就是总的方法数
         for (int i = 1; i < array.length; i++) {
             for (int j = 1; j <= target; j++) {
                 int num = 0;
