d39a3d92ec39b60fa5899f983a7a6a5e6ac875d6
wangzi <ustcwangzi@foxmail.com>
2019-05-19 14:45:37 +0800

    CoinsWayTest

--- src/test/java/com/wz/CoinsWayTest.java
+++ src/test/java/com/wz/CoinsWayTest.java
@@ -30,24 +30,26 @@ public class CoinsWayTest {
         for (int j = 1; j * array[0] <= target; j++) {
             dp[0][j * array[0]] = 1;
         }
-        // 组成dp[i][j]，可以不使用array[i]，此时方法数为dp[i-1][j]
-        // 只使用一张array[i]，此时方法数为dp[i-1][j-array[i]]
-        // 只使用k张array[i]，此时方法数为dp[i-1][j-k*array[i]]
-        // 以上各种可能相加，就是总的方法数
+        // 组成dp[i][j]，可以不使用array[i]，此时方法数为dp[i-1][j]；只使用一张array[i]，此时方法数为dp[i-1][j-array[i]]
+        // 只使用k张array[i]，此时方法数为dp[i-1][j-k*array[i]]；以上各种可能相加，就是总的方法数
+        // 即 dp[i][j] = dp[i-1][j] + dp[i-1][j-array[i]] + dp[i-1][j-2*array[i]] + ... + dp[i-1][j-k*array[i]]
+        // 其中 dp[i-1][j-array[i]] + dp[i-1][j-2*array[i]] + ... + dp[i-1][j-k*array[i]] 将j-array[i]看作y 则 =
+        // dp[i-1][y] + dp[i-1][y-array[i]] + ... + dp[i-1][y-h*array[i]] (y-h*array[i]>=0) 刚好是dp[i][y]
+        // 即 = dp[i][y] = dp[i][j-array[i]] (j-array[i]>=0)
+        // 因此 dp[i][j] = dp[i-1][j] + dp[i][j-array[i]] (j-array[i]>=0)
         for (int i = 1; i < array.length; i++) {
             for (int j = 1; j <= target; j++) {
-                int num = 0;
-                for (int k = 0; k * array[i] <= j; k++) {
-                    num += dp[i - 1][j - k * array[i]];
+                dp[i][j] = dp[i - 1][j];
+                if (j - array[i] >= 0) {
+                    dp[i][j] += dp[i][j - array[i]];
                 }
-                dp[i][j] = num;
             }
         }
         return dp[array.length - 1][target];
     }
 
     public static void main(String[] args) {
-        int times = 5;
+        int times = 20;
         boolean result = true;
         for (int i = 0; i < times; i++) {
             int[] array = RandomUtils.genRandomArrayNoRepeat(i + 1);
