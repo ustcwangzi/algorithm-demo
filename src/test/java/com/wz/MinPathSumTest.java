b0c577b05a76f8e75eca2b168c3717f20d8720a7
wangzi <ustcwangzi@foxmail.com>
2019-05-21 07:56:16 +0800

    MinPathSumTest

--- src/test/java/com/wz/MinPathSumTest.java
+++ src/test/java/com/wz/MinPathSumTest.java
@@ -12,16 +12,20 @@ import java.util.Arrays;
 public class MinPathSumTest {
     private static int solution(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
+        // dp[i][j]表示走到matrix[i][j]时的最小路径和
         int[][] dp = new int[m][n];
         dp[0][0] = matrix[0][0];
 
+        // 第一列，只能向下走
         for (int i = 1; i < m; i++) {
             dp[i][0] = dp[i - 1][0] + matrix[i][0];
         }
+        // 第一行，只能向右走
         for (int j = 1; j < n; j++) {
             dp[0][j] = dp[0][j - 1] + matrix[0][j];
         }
 
+        // 其他位置，取向下走、向右走中的最小值
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + matrix[i][j];
