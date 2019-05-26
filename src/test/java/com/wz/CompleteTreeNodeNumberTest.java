ded29ba8ad09f6181acaf60affcff37d5decfc29
wangzi <ustcwangzi@foxmail.com>
2019-05-26 10:02:34 +0800

    CompleteTreeNodeNumberTest

--- src/test/java/com/wz/CompleteTreeNodeNumberTest.java
+++ src/test/java/com/wz/CompleteTreeNodeNumberTest.java
@@ -27,14 +27,22 @@ public class CompleteTreeNodeNumberTest {
         if (head == null) {
             return 0;
         }
+        // 树高height
         int height = getMostLeftLevel(head, 1);
         return getCount(head, 1, height);
     }
 
+    /**
+     * 以node为根结点、从level层开始递归获取节点数
+     */
     private static int getCount(Node node, int level, int height) {
+        // 只有根结点一个节点
         if (level == height) {
             return 1;
         }
+        // 右子树高度与height相等，说明左子树为满二叉树，且高度为height-level
+        // 高度为h的满二叉树节点数为2^(h-1) - 1，加上node本身，节点数为2^(h-1)，然后递归获取右子树的节点数；
+        // 否则，右子树为满二叉树，且高度为height-level-1，然后递归获取左子树的节点数
         if (getMostLeftLevel(node.right, level + 1) == height) {
             return (1 << (height - level)) + getCount(node.right, level + 1, height);
         } else {
@@ -42,6 +50,9 @@ public class CompleteTreeNodeNumberTest {
         }
     }
 
+    /**
+     * 以node为根结点的树高
+     */
     private static int getMostLeftLevel(Node node, int level) {
         while (node != null) {
             level++;
