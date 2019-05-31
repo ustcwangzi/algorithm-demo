5e60c7ffaa892a90f70ef93ecfd26ecbf34d7c60
wangzi <ustcwangzi@foxmail.com>
2019-05-31 08:21:53 +0800

    MaxDistanceInTreeTest

--- src/test/java/com/wz/MaxDistanceInTreeTest.java
+++ src/test/java/com/wz/MaxDistanceInTreeTest.java
@@ -21,16 +21,29 @@ public class MaxDistanceInTreeTest {
         return getMaxDistance(head, depth);
     }
 
-    private static int getMaxDistance(Node cur, int[] depth) {
-        if (cur == null) {
+    /**
+     * 一棵以head为头节点的树上，最大距离只可能来自以下三种情况：
+     * 1、head的左子树上节点间的最大距离
+     * 2、head的右子树上节点间的最大距离
+     * 3、head左子树最大深度 + 1 + head右子树最大深度
+     * 递归函数返回节点间的最大距离，最大深度通过depth[]数组引用的方式获取结果
+     */
+    private static int getMaxDistance(Node head, int[] depth) {
+        if (head == null) {
             depth[0] = 0;
             return 0;
         }
-        int leftMaxDistance = getMaxDistance(cur.left, depth);
+        // 递归左子树获取左子树节点间最大距离
+        int leftMaxDistance = getMaxDistance(head.left, depth);
+        // 通过数组获取左子树递归过程中统计出的子树深度
         int leftMaxDepth = depth[0];
-        int rightMaxDistance = getMaxDistance(cur.right, depth);
+        // 递归右子树获取右子树节点间最大距离
+        int rightMaxDistance = getMaxDistance(head.right, depth);
+        // 通过数组获取右子树递归过程中统计出的子树深度
         int rightMaxDepth = depth[0];
+        // 通过数组记录当前结点的高度
         depth[0] = Math.max(leftMaxDepth, rightMaxDepth) + 1;
+        // 三种情况中的最大值就是以当前节点为根的树中节点间最大距离
         return Math.max(Math.max(leftMaxDistance, rightMaxDistance), leftMaxDepth + rightMaxDepth + 1);
     }
 
