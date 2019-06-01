2817f586c2d2412dd4f6547fe84dcc7885c9051c
wangzi <ustcwangzi@foxmail.com>
2019-06-01 10:27:51 +0800

    NearestCommonAncestorTest

--- src/test/java/com/wz/NearestCommonAncestorTest.java
+++ src/test/java/com/wz/NearestCommonAncestorTest.java
@@ -40,12 +40,21 @@ public class NearestCommonAncestorTest {
         }
     }
 
+    /**
+     * 整体采用后续遍历，在左右子树中寻找self和other，找到则返回，找不到返回null
+     * 假设左子树中寻找结果为left，右子树中寻找结果为right
+     * 若left和right都不为null，说明当前的头节点head就是其最近公共祖先
+     * 若left和right有一个为null，另一个不是null，那么不为null的肯定是self和other中的一个
+     * 若left和right都是null，说明未找到self和other
+     */
     private static Node solution(Node head, Node self, Node other) {
         if (head == null || head.equals(self) || head.equals(other)) {
             return head;
         }
 
+        // 在右子树中寻找self和other
         Node left = solution(head.left, self, other);
+        // 在右子树中寻找self和other
         Node right = solution(head.right, self, other);
 
         if (left != null && right != null) {
