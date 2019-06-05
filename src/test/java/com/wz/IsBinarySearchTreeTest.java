ce74cb4eb752609bfa050650587702586f00b6ea
wangzi <ustcwangzi@foxmail.com>
2019-06-06 07:56:47 +0800

    IsBinarySearchTreeTest

--- src/test/java/com/wz/IsBinarySearchTreeTest.java
+++ src/test/java/com/wz/IsBinarySearchTreeTest.java
@@ -22,6 +22,10 @@ public class IsBinarySearchTreeTest {
         return isBinarySearchTree(head, pre);
     }
 
+    /**
+     * 搜索二叉树中序遍历结果依次递增，如果遍历中出现降序，说明不是搜索二叉树
+     * 递归返回结果为"是否依次递增"，另外，利用数组pre[]记录中序遍历的前一个值，用以与当前值比较
+     */
     private static boolean isBinarySearchTree(Node head, int[] pre) {
         if (head == null) {
             return true;
@@ -29,10 +33,13 @@ public class IsBinarySearchTreeTest {
         if (!isBinarySearchTree(head.left, pre)) {
             return false;
         }
+
+        // 出现降序
         if (head.value < pre[0]) {
             return false;
         }
         pre[0] = head.value;
+
         return isBinarySearchTree(head.right, pre);
     }
 
