b58c7971bc75ef0198787ff752db472575e91db9
wangzi <ustcwangzi@foxmail.com>
2019-06-10 08:01:47 +0800

    PrintBinaryTreeByLevelTest

--- src/test/java/com/wz/PrintBinaryTreeByLevelTest.java
+++ src/test/java/com/wz/PrintBinaryTreeByLevelTest.java
@@ -3,6 +3,11 @@ package com.wz;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * <p>按层输出二叉树</p>
+ *
+ * @author wangzi
+ */
 public class PrintBinaryTreeByLevelTest {
     private static class Node {
         public int value;
@@ -14,18 +19,25 @@ public class PrintBinaryTreeByLevelTest {
         }
     }
 
+    /**
+     * 采用队列的先进先出实现层次遍历，使用last用以控制换行
+     */
     private static void solution(Node head) {
         if (head == null) {
             return;
         }
+
         Queue<Node> queue = new LinkedList<>();
         queue.offer(head);
+        // 当前行的最右节点、下一行的最右节点
         Node last = head, nextLast = null;
+
         int level = 1;
         System.out.print("Level " + (level++) + " : ");
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
             System.out.print(cur.value + " ");
+
             if (cur.left != null) {
                 queue.offer(cur.left);
                 nextLast = cur.left;
@@ -34,6 +46,8 @@ public class PrintBinaryTreeByLevelTest {
                 queue.offer(cur.right);
                 nextLast = cur.right;
             }
+
+            // 需要换行
             if (last == cur && !queue.isEmpty()) {
                 System.out.println();
                 System.out.print("Level " + (level++) + " : ");
