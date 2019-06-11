2799578d5e68b2c46e5f42acd8a5db4361aecb13
wangzi <ustcwangzi@foxmail.com>
2019-06-11 08:01:59 +0800

    PrintBinaryTreeByZigZagTest

--- src/test/java/com/wz/PrintBinaryTreeByZigZagTest.java
+++ src/test/java/com/wz/PrintBinaryTreeByZigZagTest.java
@@ -19,6 +19,11 @@ public class PrintBinaryTreeByZigZagTest {
         }
     }
 
+    /**
+     * 采用双端队列的实现层次遍历，使用last控制换行，使用forward控制打印方向
+     * 从左到右打印，则从队列头部弹出节点、从尾部加入节点，加入时先左孩子、后右孩子
+     * 从右到左打印，则从队列尾部弹出节点、从头部加入节点，加入时先右孩子、后左孩子
+     */
     private static void solution(Node head) {
         if (head == null) {
             return;
@@ -26,6 +31,7 @@ public class PrintBinaryTreeByZigZagTest {
         Deque<Node> deque = new LinkedList<>();
         deque.offerLast(head);
         boolean forward = true;
+        // 当前行最后输出的节点、下一行最后输出的节点
         Node last = head, nextLast = null;
 
         int level = 1;
@@ -33,9 +39,11 @@ public class PrintBinaryTreeByZigZagTest {
 
         while (!deque.isEmpty()) {
             Node cur;
+            // 头出尾入、先左后右
             if (forward) {
                 cur = deque.pollFirst();
                 if (cur.left != null) {
+                    // 下一行最后输出的节点是当前行有孩子的节点中最先加入queue的
                     nextLast = nextLast == null ? cur.left : nextLast;
                     deque.offerLast(cur.left);
                 }
@@ -44,6 +52,7 @@ public class PrintBinaryTreeByZigZagTest {
                     deque.offerLast(cur.right);
                 }
             } else {
+                // 尾出头入、先右后左
                 cur = deque.pollLast();
                 if (cur.right != null) {
                     nextLast = nextLast == null ? cur.right : nextLast;
@@ -56,6 +65,7 @@ public class PrintBinaryTreeByZigZagTest {
             }
 
             System.out.print(cur.value + " ");
+            // 需要换行
             if (cur == last && !deque.isEmpty()) {
                 forward = !forward;
                 last = nextLast;
