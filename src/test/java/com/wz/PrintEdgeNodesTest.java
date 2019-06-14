bf24e104fbd2d0b338f468e6d9d04aa706510ded
wangzi <ustcwangzi@foxmail.com>
2019-06-14 08:09:30 +0800

    PrintEdgeNodesTest

--- src/test/java/com/wz/PrintEdgeNodesTest.java
+++ src/test/java/com/wz/PrintEdgeNodesTest.java
@@ -1,5 +1,10 @@
 package com.wz;
 
+/**
+ * <p>头节点、叶节点以及每一层的最左最右节点为边界节点，逆时针打印二叉树的边界节点</p>
+ *
+ * @author wangzi
+ */
 public class PrintEdgeNodesTest {
     private static class Node {
         public int value;
@@ -15,13 +20,20 @@ public class PrintEdgeNodesTest {
         if (head == null) {
             return;
         }
+        // 树高
         int height = getHeight(head, 0);
+        // 记录每一层的最左、最右节点
         Node[][] edges = new Node[height][2];
+        // 填充每一层的最左、最右节点
         fillEdges(head, 0, edges);
+
+        // 打印左边界
         for (Node[] nodes : edges) {
             System.out.print(nodes[0].value + " ");
         }
+        // 打印叶节点
         printLeafNode(head, 0, edges);
+        // 打印右边界
         for (int i = height - 1; i >= 0; i--) {
             if (edges[i][0] != edges[i][1]) {
                 System.out.print(edges[i][1].value + " ");
