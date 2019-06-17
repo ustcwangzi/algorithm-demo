da9d259765685156137a2122d3408cb39e4a50f5
wangzi <ustcwangzi@foxmail.com>
2019-06-17 08:08:43 +0800

    RelocateLinkedListTest

--- src/test/java/com/wz/RelocateLinkedListTest.java
+++ src/test/java/com/wz/RelocateLinkedListTest.java
@@ -1,49 +1,25 @@
-/**
- * <p>Title: RelocateLinkedList</p>
- * <p>Description: </p>
- * <p>Created by wangzi on 2018/10/14</p>
- * <p>Emil: ustcwangzi@foxmail.com</p>
- * <p>WebSite: https://github.com/ustcwangzi/</p>
- */
-package com.wz.list;
+package com.wz;
 
-/**
- * <p>按照左右半区重新组合单链表</p>
- * <p>
- *     将链表分为左右半区，左半区为L1->L2->...，右半区为R1->R2->...，调整为L1->R1->L2->R2->...
- *     解决过程：
- *          1、找到左半区最后一个节点mid
- *          2、根据mid，将左右半区分离成两个链表
- *          3、将两个链表按照给定要求合并起来
- *     时间复杂度为O(N)，空间复杂度为O(1)
- * </p>
- *
- * @author wangzi
- */
-public class RelocateLinkedList {
+public class RelocateLinkedListTest {
     private static class Node {
         public int value;
         public Node next;
 
-        public Node(int value) {
+        private Node(int value) {
             this.value = value;
         }
     }
 
-    public static void relocate(Node head) {
+    private static void solution(Node head) {
         if (head == null || head.next == null) {
             return;
         }
-
-        // 中间节点
         Node mid = head;
         Node right = head.next;
         while (right.next != null && right.next.next != null) {
             mid = mid.next;
             right = right.next.next;
         }
-
-        // 右半部分开始节点
         right = mid.next;
         mid.next = null;
         merge(head, right);
@@ -58,7 +34,6 @@ public class RelocateLinkedList {
             left = right.next;
             right = next;
         }
-        // 最后一个right节点直接放后面
         left.next = right;
     }
 
@@ -69,7 +44,7 @@ public class RelocateLinkedList {
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
 
-        relocate(head);
+        solution(head);
         while (head != null) {
             System.out.print(head.value + " ");
             head = head.next;
