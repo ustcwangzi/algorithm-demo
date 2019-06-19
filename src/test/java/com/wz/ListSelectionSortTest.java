7933f6e8aa6e1e5f37fe59f03654f0cda25bea72
wangzi <ustcwangzi@foxmail.com>
2019-06-20 07:56:23 +0800

    ListSelectionSortTest

--- src/test/java/com/wz/ListSelectionSortTest.java
+++ src/test/java/com/wz/ListSelectionSortTest.java
@@ -1,5 +1,10 @@
 package com.wz;
 
+/**
+ * <p>对链表进行选择排序</p>
+ *
+ * @author wangzi
+ */
 public class ListSelectionSortTest {
     private static class Node {
         public int value;
@@ -14,32 +19,44 @@ public class ListSelectionSortTest {
         if (head == null || head.next == null) {
             return head;
         }
+
+        // 已排序链表的尾节点
         Node tail = null;
         Node cur = head;
+        // 未排序链表中最小节点及其前一个节点
         Node small, smallPre;
         while (cur != null) {
             small = cur;
+            // 获取最小节点的前一个节点
             smallPre = getSmallestPreNode(cur);
             if (smallPre != null) {
+                // 将最小节点从未排序链表中去除
                 small = smallPre.next;
                 smallPre.next = small.next;
             }
 
             cur = cur == small ? cur.next : cur;
+            // 将最小节点加入到已排序链表的尾部
             if (tail == null) {
                 head = small;
             } else {
                 tail.next = small;
             }
+            // 更新已排序链表的尾节点
             tail = small;
         }
         return head;
     }
 
+    /**
+     * 找到以head为头节点的链表中最小节点的前一个节点
+     */
     private static Node getSmallestPreNode(Node head) {
         if (head == null || head.next == null) {
             return null;
         }
+
+        // 最小节点及其前一个节点
         Node small = head, smallPre = null;
         Node pre = head, cur = head.next;
         while (cur != null) {
@@ -50,6 +67,7 @@ public class ListSelectionSortTest {
             pre = cur;
             cur = cur.next;
         }
+
         return smallPre;
     }
 
