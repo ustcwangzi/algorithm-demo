a394860c2a188eb22baa9eac95475210b1f06a46
wangzi <ustcwangzi@foxmail.com>
2019-06-22 10:50:58 +0800

    ConvertEveryGroupTest

--- src/test/java/com/wz/ConvertEveryGroupTest.java
+++ src/test/java/com/wz/ConvertEveryGroupTest.java
@@ -8,7 +8,7 @@
 package com.wz;
 
 /**
- * <p></p>
+ * <p>单链表每K个节点为一组，组内逆序，最后不足K个节点时，保持不变</p>
  *
  * @author wangzi
  */
@@ -33,9 +33,12 @@ public class ConvertEveryGroupTest {
             next = cur.next;
             if (count == k) {
                 start = left == null ? head : left.next;
+                // 首次逆置时改变链表头节点
                 head = left == null ? cur : head;
+                // 逆置[start,cur]，位于left与next之间
                 reverse(left, start, cur, next);
                 left = start;
+                // 下面会执行count++，因此此处count置0
                 count = 0;
             }
             count++;
@@ -44,8 +47,12 @@ public class ConvertEveryGroupTest {
         return head;
     }
 
+    /**
+     * 逆置[start,end]，逆置后以left为队首，right为队尾
+     */
     private static void reverse(Node left, Node start, Node end, Node right) {
         Node pre = start;
+        // 每次需要逆置的节点，及其下一个节点
         Node cur = pre.next, next;
         while (cur != right) {
             next = cur.next;
@@ -53,6 +60,7 @@ public class ConvertEveryGroupTest {
             pre = cur;
             cur = next;
         }
+        // start到end逆置完成，与left、right连接在一起
         if (left != null) {
             left.next = end;
         }
@@ -69,6 +77,15 @@ public class ConvertEveryGroupTest {
         head.next.next.next.next.next.next = new Node(7);
 
         head = solution(head, 3);
+        Node cur = head;
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+
+        System.out.println();
+
+        head = solution(head, 2);
         while (head != null) {
             System.out.print(head.value + " ");
             head = head.next;
