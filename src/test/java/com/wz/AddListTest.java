7457e0198f7d34d1efba2a5f8776a8ef21c151a8
wangzi <ustcwangzi@foxmail.com>
2019-06-23 10:00:26 +0800

    AddListTest

--- src/test/java/com/wz/AddListTest.java
+++ src/test/java/com/wz/AddListTest.java
@@ -8,7 +8,7 @@
 package com.wz;
 
 /**
- * <p></p>
+ * <p>把链表看作整型数值，将两个链表相加，结果生成新的链表</p>
  *
  * @author wangzi
  */
@@ -22,18 +22,24 @@ public class AddListTest {
         }
     }
 
+    /**
+     * 将两个链表逆序，然后遍历链表，值相加，用carry记录进位
+     */
     private static Node solution(Node self, Node other) {
         if (self == null || other == null) {
             return self == null ? other : self;
         }
         self = reverse(self);
         other = reverse(other);
+
+        // 每个链表的节点值、相加结果以及进位
         int num1, num2, count, carry = 0;
         Node cur1 = self, cur2 = other, cur = null, pre;
         while (cur1 != null || cur2 != null) {
             num1 = cur1 != null ? cur1.value : 0;
             num2 = cur2 != null ? cur2.value : 0;
             count = num1 + num2 + carry;
+            // 相加是从低位向高位进行的，因为需要逆序生成新链表
             pre = cur;
             cur = new Node(count % 10);
             carry = count / 10;
@@ -42,6 +48,7 @@ public class AddListTest {
             cur1 = cur1 != null ? cur1.next : null;
             cur2 = cur2 != null ? cur2.next : null;
         }
+        // 最后一个进位单独生成节点
         if (carry == 1) {
             pre = cur;
             cur = new Node(1);
@@ -54,6 +61,9 @@ public class AddListTest {
         return cur;
     }
 
+    /**
+     * 链表逆序
+     */
     private static Node reverse(Node head) {
         if (head == null || head.next == null) {
             return head;
