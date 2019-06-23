a4ed427dcf23c699b6f4768cd34c78a43ef7c13c
wangzi <ustcwangzi@foxmail.com>
2019-06-24 07:58:14 +0800

    IsPalindromeListTest

--- src/test/java/com/wz/IsPalindromeListTest.java
+++ src/test/java/com/wz/IsPalindromeListTest.java
@@ -2,6 +2,11 @@ package com.wz;
 
 import java.util.Stack;
 
+/**
+ * <p>判断一个链表是否为回文结构</p>
+ *
+ * @author wangzi
+ */
 public class IsPalindromeListTest {
     private static class Node {
         public int value;
@@ -12,23 +17,29 @@ public class IsPalindromeListTest {
         }
     }
 
+    /**
+     * 对右半部分节点进行压栈，出栈时和左半部分节点相比较
+     */
     private static boolean solution(Node head) {
         if (head == null || head.next == null) {
             return true;
         }
 
+        // right指向右半区的开始
         Node cur = head, right = head.next;
         while (cur.next != null && cur.next.next != null) {
             right = right.next;
             cur = cur.next.next;
         }
 
+        // 将右半区入栈
         Stack<Node> stack = new Stack<>();
         while (right != null) {
             stack.push(right);
             right = right.next;
         }
 
+        // 右半区出栈，与左半区相比较
         while (!stack.isEmpty()) {
             if (head.value != stack.pop().value) {
                 return false;
