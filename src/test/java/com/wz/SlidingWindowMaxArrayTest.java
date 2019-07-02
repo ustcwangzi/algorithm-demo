bc0571435d5ac8637e066f8842b83cdd82662e0f
wangzi <ustcwangzi@foxmail.com>
2019-07-02 08:26:31 +0800

    SlidingWindowMaxArrayTest

--- src/test/java/com/wz/SlidingWindowMaxArrayTest.java
+++ src/test/java/com/wz/SlidingWindowMaxArrayTest.java
@@ -7,7 +7,17 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Random;
 
+/**
+ * <p>生成窗口最大值数组</p>
+ *
+ * @author wangzi
+ */
 public class SlidingWindowMaxArrayTest {
+    /**
+     * 使用双端队列来维持当前的最大值，队首一直是当前窗口的最大值下标
+     * 若当前元素大于等于队尾则弹出队尾，最后将当前元素入队；否则直接入队
+     * 队首元素等于i-size，说明当前队首已过期，弹出队首
+     */
     private static int[] solution(int[] array, int size) {
         if (array == null || size <= 1 || array.length < size) {
             return array;
@@ -17,13 +27,17 @@ public class SlidingWindowMaxArrayTest {
         int[] result = new int[array.length - size + 1];
         int index = 0;
         for (int i = 0; i < array.length; i++) {
+            // 保证新进的值小于等于队尾
             while (!deque.isEmpty() && array[deque.peekLast()] <= array[i]) {
                 deque.pollLast();
             }
             deque.addLast(i);
+
+            // 队首已过期，弹出
             if (deque.peekFirst() == i - size) {
                 deque.pollFirst();
             }
+            // 记录当前窗口最大值，即队首对应的元素值
             if (i >= size - 1) {
                 result[index++] = array[deque.peekFirst()];
             }
