55de526e039b0a9d50d1a5d8b3240532f077a9e7
wangzi <ustcwangzi@foxmail.com>
2019-07-04 07:49:40 +0800

    ReverseStackUsingRecursiveTest

--- src/test/java/com/wz/ReverseStackUsingRecursiveTest.java
+++ src/test/java/com/wz/ReverseStackUsingRecursiveTest.java
@@ -2,8 +2,18 @@ package com.wz;
 
 import java.util.Stack;
 
+/**
+ * <p>使用递归将栈逆置</p>
+ *
+ * @author wangzi
+ */
 public class ReverseStackUsingRecursiveTest {
 
+    /**
+     * 将栈逆置
+     * 以3、2、1为例说明该过程：
+     * lastElement=1、lastElement=2、lastElement=3，栈空返回，将3、2、1压入栈中
+     */
     private static void solution(Stack<Integer> stack) {
         if (stack.isEmpty()) {
             return;
@@ -13,6 +23,11 @@ public class ReverseStackUsingRecursiveTest {
         stack.push(lastElement);
     }
 
+    /**
+     * 获取并移除栈底元素
+     * 以3、2、1为例说明该过程：
+     * result=3、result=2、result=1，栈空返回1，lastElement=1，将2、3压入栈中，返回1
+     */
     private static int getAndRemoveLastElement(Stack<Integer> stack) {
         int result = stack.pop();
         if (stack.isEmpty()) {
@@ -21,5 +36,16 @@ public class ReverseStackUsingRecursiveTest {
         int lastElement = getAndRemoveLastElement(stack);
         stack.push(result);
         return lastElement;
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        solution(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 }
