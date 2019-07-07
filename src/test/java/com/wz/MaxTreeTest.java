d325aaefbe8aba628e95612d0db05a070f5156c1
wangzi <ustcwangzi@foxmail.com>
2019-07-07 10:47:02 +0800

    MaxTreeTest

--- src/test/java/com/wz/MaxTreeTest.java
+++ src/test/java/com/wz/MaxTreeTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import java.util.Stack;
 
 /**
- * <p></p>
+ * <p>构造数组的MaxTree，数组中无重复元素</p>
  *
  * @author wangzi
  */
@@ -27,6 +27,14 @@ public class MaxTreeTest {
         }
     }
 
+    /**
+     * 对每一个元素，从左边和右边各选择第一个比该元素大的值，选择其中较小的作为父节点
+     * 若左边和右边都没有比它大的，说明该节点就是整个树的根结点
+     * 利用栈找每一个元素左边和右边第一个比它大的元素：
+     * 正向遍历数组，若当前元素i大于栈顶j，弹出j，弹出后栈顶为k，说明j左边第一个比它大的元素就是k，记录该关系，然后i入栈
+     * 数组为空时，若栈不空，则依次出栈，弹出j，弹出后栈顶为k，说明j左边第一个比它大的元素就是k，记录该关系，然后继续出栈
+     * 逆向遍历数组，以同样的方式，可以获取每一个元素右边第一个比它的大元素
+     */
     private static Node solution(int[] array) {
         if (array == null || array.length == 0) {
             return null;
@@ -37,12 +45,15 @@ public class MaxTreeTest {
         for (int i = 0; i < length; i++) {
             nodeArray[i] = new Node(array[i]);
         }
+        // 每一个节点左侧第一个比它大的节点
         Map<Node, Node> leftBigMap = new HashMap<>(length);
+        // 每一个节点右侧第一个比它大的节点
         Map<Node, Node> rightBigMap = new HashMap<>(length);
 
         fillBigMap(nodeArray, leftBigMap, rightBigMap);
 
         Node head = null;
+        // 构造二叉树
         for (Node cur : nodeArray) {
             Node left = leftBigMap.get(cur);
             Node right = rightBigMap.get(cur);
@@ -74,6 +85,7 @@ public class MaxTreeTest {
 
     private static void fillBigMap(Node[] array, Map<Node, Node> leftBigMap, Map<Node, Node> rightBigMap) {
         Stack<Node> stack = new Stack<>();
+        // 正向遍历，获取当前节点左边第一个比自己大的元素
         for (Node cur : array) {
             while (!stack.isEmpty() && stack.peek().value < cur.value) {
                 popStackSetMap(stack, leftBigMap);
@@ -84,6 +96,7 @@ public class MaxTreeTest {
             popStackSetMap(stack, leftBigMap);
         }
 
+        // 逆向遍历，获取当前节点右边第一个比自己大的元素
         for (int i = array.length - 1; i > -1; i--) {
             Node cur = array[i];
             while (!stack.isEmpty() && stack.peek().value < cur.value) {
@@ -96,6 +109,9 @@ public class MaxTreeTest {
         }
     }
 
+    /**
+     * 栈顶出栈，同时设置第一个比自己大的元素
+     */
     private static void popStackSetMap(Stack<Node> stack, Map<Node, Node> bigMap) {
         Node cur = stack.pop();
         if (!stack.isEmpty()) {
